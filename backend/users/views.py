032cfd7398b19ee5848d591a4ff5ba81e3954936
Viva <sovestyaaaa@gmail.com>
2023-09-23 20:33:38 +0300

    changed imports

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import action
 
 from .models import Follow
 from .serializers import UserFollowSerializer
