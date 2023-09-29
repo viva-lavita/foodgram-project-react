8e9027e94ad34f109db63416215e4a2fc388da76
Viva <sovestyaaaa@gmail.com>
2023-09-29 17:03:47 +0300

    change admin users

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
 from .models import Follow
 
@@ -32,6 +33,25 @@ class CustomUserAdmin(UserAdmin):
     inlines = [
         SubscriptionInline,
     ]
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    filter_horizontal = (
+        'user_permissions',
+    )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
